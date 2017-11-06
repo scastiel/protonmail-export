2a13a16e043688acc1c52acd26bdcb323e266a03
FrontierPsycho <nonsenseisay@protonmail.com>
2017-11-06 20:57:06 +0100

    Updated x-pm-appversion to Web_3.11.7.
    Otherwise ProtonMail throws an error even though the code is still
    working.

--- src/ConversationFetcher.js
+++ src/ConversationFetcher.js
@@ -13,7 +13,7 @@ export default class ConversationsFetcher {
         this._headers = {
             cookie,
             'x-pm-apiversion': '1',
-            'x-pm-appversion': 'Web_3.7.2',
+            'x-pm-appversion': 'Web_3.11.7',
             'x-pm-session': sessionId
         }
         this._privateKey = privateKey;
